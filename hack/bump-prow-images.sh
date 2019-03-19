d346f47087fc1515174a0901a15982043650253d
Steve Kuznetsov <skuznets@redhat.com>
2019-03-19 11:40:26 -0700

    Get ghproxy image fom k8s-prow repo
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/bump-prow-images.sh
+++ hack/bump-prow-images.sh
@@ -30,7 +30,7 @@ done
 
 # some images are in a different bucket and are pushed in a different cadence
 # and are not necessarily in order, so we need to do some silliness
-for image in 'label_sync' 'commenter' 'ghproxy'; do
+for image in 'label_sync' 'commenter'; do
 	current_tag="$( grep -Pho "(?<=${image}:)v[0-9]{8}-[a-z0-9]+" $( find cluster/ci/config/prow/openshift -type f ) ci-operator/jobs/infra-periodics.yaml | head -n 1 )"
 	latest_tag="$( gcloud container images list-tags gcr.io/k8s-testimages/${image} --format='value(tags)' --limit 100 | grep "latest," | grep -Po "v[0-9]+\-[a-z0-9]+" )"
 	if [[ "${current_tag}" != "${latest_tag}" ]]; then
